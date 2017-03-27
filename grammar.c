08844e89a1f54e542fce0275c3fbc4b862bcc8c4
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-27 13:56:20 +0200

    basic grammar

---
+++ grammar.c
@@ -0,0 +1,17 @@
+mpc_parser_t* Adjective = mpc_or(4,
+  mpc_sym("wow"), mpc_sym("many"),
+  mpc_sym("so"), mpc_sym("such")
+);
+
+mpc_parser_t* Noun = mpc_or(5,
+  mpc_sym("lisp"), mpc_sym("language"),
+  mpc_sym("book"), mpc_sym("build"),
+  mpc_sym("c")
+);
+
+// phrase, for joining adjectives and nouns
+mpc_parser_t* Phrase = mpc_and(2, mpcf_strfold,
+  Adjective, Noun, free
+);
+
+// join phrases to get a Doge
