088674b4bcf66841a30ea5d604b8144331e14783
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-27 11:51:56 +0200

    add REPL prompt

--- repl.c
+++ repl.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
-int main() {
-  printf("Welcome to Morpheus");
+static char input[2048];
+
+int main(int argc, char** argv) {
+  printf("Welcome to Morpheus 0.1");
+  puts("Press Ctrl+C to Exit\n");
 }
