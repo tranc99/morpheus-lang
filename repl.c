39eef2fb7e70c4f40887df5a467c5ae3cdff987c
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-27 12:01:40 +0200

    repl

--- repl.c
+++ repl.c
@@ -5,4 +5,12 @@ static char input[2048];
 int main(int argc, char** argv) {
   printf("Welcome to Morpheus 0.1");
   puts("Press Ctrl+C to Exit\n");
+
+  while(1) {
+    fputs("morpheus> ", stdout);
+    fgets(input, 2048, stdin);
+    printf("No you're a %s", input);
+  }
+
+  return 0;
 }
