8f737707f5969b5760604f0e687714357473e382
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-27 12:43:48 +0200

    add preprocessor directives

--- prompt.c
+++ prompt.c
@@ -1,15 +1,36 @@
 #include<stdio.h>
 #include <stdlib.h>
 
+// If on Windows, compile these functions
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {
+
+}
+
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
   printf("Welcome to Morpheus 0.1");
   puts("Press Ctrl+C to Exit\n");
 
   while(1) {
-    char* input = readline("morpheus> ");
+    char* input = readline("morpheus>");
     add_history(input);
     printf("No you're a %s\n", input);
     free(input);
