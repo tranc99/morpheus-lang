9496f8710644c07079e4abd7e5803a6711a2feab
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-28 22:59:51 +0200

    carry out evaluation

--- evaluation.c
+++ evaluation.c
@@ -26,6 +26,39 @@ void add_history(char* unused) {
 #include <editline/history.h>
 #endif
 
+// perform a matched operation and return the result
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+// define a recursive eval
+long eval(mpc_ast_t* t) {
+  // return a number directly
+  if(strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  // the operator is always second child
+  char* op = t->children[1]->contents;
+
+  // store the third child in 'x'
+  long x = eval(t->children[2]);
+
+  // iterate the remaining children and combining
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+
 int main(int argc, char** argv) {
 
   // parsers for different parts of the grammar
@@ -53,8 +86,10 @@ int main(int argc, char** argv) {
     // parse the user input
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      // on success print the AST
-      mpc_ast_print(r.output);
+      // on success evaluate the AST
+      // mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       // otherwise print the error
