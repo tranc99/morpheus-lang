dba883542ce99d309ec30185e0faa040e2c96bcb
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-28 17:40:13 +0200

    create Grammar definitions

--- parsing.c
+++ parsing.c
@@ -27,6 +27,23 @@ void add_history(char* unused) {
 #endif
 
 int main(int argc, char** argv) {
+
+  // parsers for different parts of the grammar
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  // define the parsers with the following Language
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                  \
+      number  : /-?[0-9]+/ ;                            \
+      operator : '+' | '-' | '*' | '/' ;               \
+      expr : <number> | '(' <operator> <expr>+ ')' ;   \
+      lispy: /^/ <operator> <expr>+ /$/;               \
+    ",
+  Number, Operator, Expr, Lispy);
+
   printf("Welcome to Morpheus 0.1");
   puts("Press Ctrl+C to Exit\n");
 
