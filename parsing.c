d43ae39d3c0e5592cdc095569339707216271a47
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-28 18:19:58 +0200

    code as art

--- parsing.c
+++ parsing.c
@@ -50,12 +50,22 @@ int main(int argc, char** argv) {
   while(1) {
     char* input = readline("morpheus> ");
     add_history(input);
-    printf("No you're a %s\n", input);
+    // parse the user input
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      // on success print the AST
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      // otherwise print the error
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
     free(input);
   }
 
   // undefine and delete our parsers
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
-  
+
   return 0;
 }
